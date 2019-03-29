ee4b079d668a5dd7ff1d6a2b941da2771cda72b3
ManlyMarco <dummyaccx@gmail.com>
2019-03-29 13:50:16 +0100

    Fixed compat issue with runtime editor

--- KKCheatTools/CheatTools.cs
+++ KKCheatTools/CheatTools.cs
@@ -4,26 +4,25 @@ using UnityEngine;
 namespace CheatTools
 {
     [BepInPlugin("CheatTools", "Cheat Tools", Version)]
+    [BepInDependency(RuntimeUnityEditor.RuntimeUnityEditor.GUID)]
     public class CheatTools : BaseUnityPlugin
     {
         public const string Version = "2.2";
         
         private CheatWindow _cheatWindow;
-
-        protected void Start()
-        {
-            _cheatWindow = new CheatWindow(GetComponent<RuntimeUnityEditor.RuntimeUnityEditor>());
-        }
-
+        
         protected void OnGUI()
         {
-            _cheatWindow.DisplayCheatWindow();
+            _cheatWindow?.DisplayCheatWindow();
         }
 
         protected void Update()
         {
             if (Input.GetKeyDown(KeyCode.F12))
             {
+                if(_cheatWindow == null)
+                    _cheatWindow = new CheatWindow(GetComponent<RuntimeUnityEditor.RuntimeUnityEditor>());
+
                 _cheatWindow.Show = !_cheatWindow.Show;
             }
         }
