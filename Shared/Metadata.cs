fbd36b8b0aebce60fe08dd68e5204bc5742b1332
ManlyMarco <dummyaccx@gmail.com>
2020-11-25 03:10:09 +0100

    Version up to 2.8.1

--- Shared/Metadata.cs
+++ Shared/Metadata.cs
@@ -3,6 +3,6 @@
     internal static class Metadata
     {
         public const string GUID = "CheatTools";
-        public const string Version = "2.8";
+        public const string Version = "2.8.1";
     }
 }
