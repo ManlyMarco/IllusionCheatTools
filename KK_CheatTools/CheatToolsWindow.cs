ae58184eacb0e52155a7a5b0d79de59f99561c3a
ManlyMarco <dummyaccx@gmail.com>
2021-02-09 21:25:23 +0100

    Refactoring

--- KK_CheatTools/CheatToolsWindow.cs
+++ KK_CheatTools/CheatToolsWindow.cs
@@ -22,48 +22,6 @@ namespace CheatTools
     public class CheatToolsWindow
     {
         private const int ScreenOffset = 20;
-        private enum HExperienceKindEng
-        {
-            FirstTime = 0,
-            Inexperienced = 1,
-            Experienced = 2,
-            Perverted = 3,
-        };
-
-        private enum DesireEng
-        {
-            ChangeClothes = 0,
-            GoToToilet = 1,
-            TakeShower = 2,
-            HaveMeal = 3,
-            HMasturbate = 4,
-            HWithPlayer = 5,
-            DoClubActivities = 6,
-            TalkWithGirls = 7,
-            TalkWithPlayer = 8,
-            Read = 9,
-            FiddleWithSmartphone = 10,
-            GameOnSmartphone = 11,
-            TakeSelfie = 12,
-            ListenToMusic = 13,
-            DancePractice = 14,
-            ImproveMyAppearance = 15,
-            Drink = 16,
-            ChangeMyMind = 17,
-            Exercise = 18,
-            CharaSpecificBehavior = 19,
-            GetAway = 20,
-            Study = 21,
-            Sleep = 22,
-            FollowMe = 23,
-            Request = 24,
-            Shy = 25,
-            Lesbian = 26,
-            LesbianPartner = 27,
-            AskPlayerToTalk = 28,
-            AskPlayerForH = 29,
-            FidgetingWaiting = 30
-        }
 
         private readonly RuntimeUnityEditorCore _editor;
 
---
+++ KK_CheatTools/DesireEng.cs
@@ -0,0 +1,37 @@
+﻿namespace CheatTools
+{
+    internal enum DesireEng
+    {
+        ChangeClothes = 0,
+        GoToToilet = 1,
+        TakeShower = 2,
+        HaveMeal = 3,
+        HMasturbate = 4,
+        HWithPlayer = 5,
+        DoClubActivities = 6,
+        TalkWithGirls = 7,
+        TalkWithPlayer = 8,
+        Read = 9,
+        FiddleWithSmartphone = 10,
+        GameOnSmartphone = 11,
+        TakeSelfie = 12,
+        ListenToMusic = 13,
+        DancePractice = 14,
+        ImproveMyAppearance = 15,
+        Drink = 16,
+        ChangeMyMind = 17,
+        Exercise = 18,
+        CharaSpecificBehavior = 19,
+        GetAway = 20,
+        Study = 21,
+        Sleep = 22,
+        FollowMe = 23,
+        Request = 24,
+        Shy = 25,
+        Lesbian = 26,
+        LesbianPartner = 27,
+        AskPlayerToTalk = 28,
+        AskPlayerForH = 29,
+        FidgetingWaiting = 30
+    }
+}
