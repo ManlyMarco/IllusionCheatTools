9853aedb3b5dc821148391f606d9e02376133b2c
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2024-02-03 14:15:37 +0100

    Add HC port (#23)

---
+++ HC_CheatTools/CheatToolsPlugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using RuntimeUnityEditor.Core;
+using Shared;
+
+namespace CheatTools
+{
+    [BepInPlugin(Metadata.GUID, "Cheat Tools", Version)]
+    [BepInDependency(RuntimeUnityEditorCore.GUID, RuntimeUnityEditorCore.Version)]
+    [BepInProcess("HoneyCome")]
+    public class CheatToolsPlugin : BasePlugin
+    {
+        public const string Version = Metadata.Version;
+
+        internal static new ManualLogSource Logger;
+
+        public CheatToolsPlugin()
+        {
+            Logger = base.Log;
+        }
+
+        public override void Load()
+        {
+            var runtimeUnityEditorCore = RuntimeUnityEditorCore.Instance;
+            if (runtimeUnityEditorCore == null)
+            {
+                Logger.Log(LogLevel.Error | LogLevel.Message, "Failed to get RuntimeUnityEditor! Make sure you don't have multiple versions of it installed!");
+                return;
+            }
+
+            CheatToolsWindowInit.InitializeCheats();
+
+            runtimeUnityEditorCore.AddFeature(new CheatToolsWindow(runtimeUnityEditorCore));
+        }
+    }
+}
