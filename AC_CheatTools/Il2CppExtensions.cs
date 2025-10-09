f72e23e165d0d76fe61d9712e5889e92275e184f
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-10-09 04:52:14 +0200

    Aicomi wip

---
+++ AC_CheatTools/Il2CppExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace IllusionMods
+{
+    internal static class Il2CppExtensions
+    {
+        public static IEnumerable<T> AsManagedEnumerable<T>(this Il2CppSystem.Collections.Generic.List<T> collection)
+        {
+            foreach (var val in collection)
+                yield return val;
+        }
+        public static IEnumerable<T> AsManagedEnumerable<T>(this Il2CppSystem.Collections.Generic.HashSet<T> collection)
+        {
+            foreach (var val in collection)
+                yield return val;
+        }
+        public static IEnumerable<KeyValuePair<T1, T2>> AsManagedEnumerable<T1, T2>(this Il2CppSystem.Collections.Generic.Dictionary<T1, T2> collection)
+        {
+            foreach (var val in collection)
+                yield return new KeyValuePair<T1, T2>(val.Key, val.Value);
+        }
+    }
+}
